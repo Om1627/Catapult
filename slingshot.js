b1be1500e229c8cb7592fbd684c2fd9d0fc268c0
Om1627 <60969676+Om1627@users.noreply.github.com>
2020-03-30 19:19:38 +0530

    Update slingshot.js

--- slingshot.js
+++ slingshot.js
@@ -6,8 +6,8 @@ class Slingshot{
             stiffness: 0.04,
             length: 10
         }
-        this.sling1= loadImage("sprites/sling1.png");
-        this.sling2= loadImage("sprites/sling2.png");
+        this.sling1= loadImage("sling1.png");
+        this.sling2= loadImage("sling2.png");
         this.pointB=pointB;
         this.sling = Constraint.create(options);
         World.add(world, this.sling);
